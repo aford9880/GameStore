da6aaad83e56a91529aaddfd13322def72953294
aford9880 <aford9880@gmail.com>
2019-10-25 12:46:21 +0300

    Added cart model

---
+++ GameStore/App_Start/RouteConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Routing;
+
+// namespace GameStore.App_Start
+namespace GameStore {
+    public class RouteConfig {
+        public static void RegisterRoutes(RouteCollection routes) {
+            routes.MapPageRoute(null, "list/{category}/{page}", "~/Pages/Listing.aspx");
+            routes.MapPageRoute(null, "list/{page}", "~/Pages/Listing.aspx");
+            routes.MapPageRoute(null, "", "~/Pages/Listing.aspx");
+            routes.MapPageRoute(null, "list", "~/Pages/Listing.aspx");
+        }
+    }
+}
--- GameStore/Pages/Listing.aspx.cs
+++ GameStore/Pages/Listing.aspx.cs
@@ -7,27 +7,40 @@ using System.Linq;
 namespace GameStore.Pages {
     public partial class Listing : System.Web.UI.Page {
         private readonly Repository _repository = new Repository();
-        private int _pageSize = 4;
+        private readonly int _pageSize = 4;
         protected int CurrentPage {
-            get {
-                int page;
-                // если не удалось преобразовать page в int, то выводим 1 страницу
-                page = int.TryParse(Request.QueryString["page"], out page /*выведет 2, если в конце url будет "?page=2"*/) ? page : 1;
+            get {                
+                int page = GetPageFromRequest();                
                 return page > MaxPage ? MaxPage : page; 
             }
         }
         // свойство, возвращающее наибольший номер допустимой страницы
         protected int MaxPage {
             get {
-                return (int)Math.Ceiling((decimal)_repository.Games.Count() / _pageSize);
+                int prodCount = FilterGames().Count();
+                return (int)Math.Ceiling((decimal)prodCount / _pageSize);
             }
         }
+        private int GetPageFromRequest() {
+            string reqValue = (string)RouteData.Values["page"] ??
+                Request.QueryString["page"];
+            return reqValue != null && int.TryParse(reqValue, out int page) ? page : 1;
+        }
         protected IEnumerable<Game> GetGames() {
-            return _repository.Games
+            return FilterGames()
                 .OrderBy(g => g.GameId)
                 .Skip((CurrentPage - 1) * _pageSize)
                 .Take(_pageSize);
         }
+        // вспомогательный метод для фильтрации игр по категориям
+        private IEnumerable<Game> FilterGames() {
+            IEnumerable<Game> games = _repository.Games;
+            string currentCategory = (string)RouteData.Values["category"] ??
+                Request.QueryString["category"];
+            return currentCategory == null ? games :
+                games.Where(p => p.Category == currentCategory);
+        }
+
         protected void Page_Load(object sender, EventArgs e) {
         }
     }
